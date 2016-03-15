1303e4699e9db4b53b3cae7e493b589f26ea1cf2
suzhou01 <suzhou01@126.com>
2016-03-15 16:33:16 +0800

    Added files via upload

---
+++ Table-Product.sql
@@ -0,0 +1,30 @@
+﻿USE [MobileDemo]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 3/15/2016 4:29:13 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Code] [varchar](30) NOT NULL,
+	[Name] [nvarchar](500) NOT NULL,
+	[UnitPrice] [float] NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
